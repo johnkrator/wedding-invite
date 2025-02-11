1d4230a5f560bc3d7fd0ba07d35d6a17af6a7fae
Chidiebere <chukwuchidieberejohn@gmail.com>
2025-02-11 20:11:25 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {LayoutComponent} from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {
+        path: 'home',
+        loadChildren: () => import('../app/pages/home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: '**', redirectTo: 'home',
+      }
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutRoutingModule {
+}
